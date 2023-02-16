322f037176112e9253fb90386f931891aa7dded0
QXS <253513374@qq.com>
2023-02-17 00:22:49 +0800

    删除必要文件

--- Weitedianlan.Web/Weitedianlan.Web/Application/ApplicationKeys.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Weitedianlan.Web.Application
-{
-    public partial class ApplicationKeys
-    {
-        public const string User_Cookie_Encryption_Key = "User_Cookie_Encryption_Key";
-
-        public const string User_Cookie_Key = "User_Cookie_Key";
-    }
-}
--- Weitedianlan.Web/Wtdl.Web.Api/Controllers/LoginViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Wtdl.Web.Api.Controllers
-{
-    public class LoginViewModel
-    {
-        public string Password { get; set; }
-        public string Username { get; set; }
-    }
-}
